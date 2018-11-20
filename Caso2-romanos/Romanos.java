8638f9e5a1ca4ee78e4ac41dfb3ee402f8ad9bd3
Alvaro Villalba <alvaro.vica@hotmail.com>
2018-11-20 12:45:58 +0100

    añado Romanos.Java

---
+++ Caso2-romanos/Romanos.java
@@ -0,0 +1,94 @@
+public class RomanNumeral {
+	// Devuelve true si año es roman, false en caso contrario
+	// No se especifica qué ocurre si el número es negativo
+	// => Se ha optado por lanzar la excepción RuntimeException
+	public int convierte(String s) {
+
+		int decimal = 0 //esta es la variable en la que vamos a ir almacenando la suma
+
+		if (s.length() <= 0) {
+			throw new RuntimeException("Longitud incorrecta");
+		}
+		String romanNumeral = s.toUpperCase(); //lo ponemos todo en myosculas para facilitarnos las cosas
+		for (int i= 0;i < romanNumeral.length() ;i++ ) {
+			//vamos a ir recorriendonos el string caracter a caracter 
+			char decimalConvert = romanNumeral.charAt(i);
+			switch (decimalConvert) {
+				case 'M':
+		            decimal += 1000;
+		            break;
+
+		        case 'D':
+		            decimal += 500;
+		            break;
+
+		        case 'C':
+		            decimal += 100;
+		            break;
+
+		        case 'L':
+		            decimal += 50;
+		            break;
+
+		        case 'X':
+		            decimal += 10;
+		            break;
+
+		        case 'V':
+		            decimal += 5;
+		            break;
+
+		        case 'I':
+		            decimal += 1;
+		            break;
+
+		        default:
+		        throw new RuntimeException("Letra incorrecta");
+        	}
+		}
+		//ahora tenemos que comprobar las restas 
+		if (romanNumeral.contains("IV"))
+	    {
+	        decimal-=2;
+	    }
+	    if (romanNumeral.contains("IX"))
+	    {
+	        decimal-=2;
+	    }
+	    if (romanNumeral.contains("XL"))
+	    {
+	        decimal-=10;
+	    }
+	    if (romanNumeral.contains("XC"))
+	    {
+	        decimal-=10;
+	    }
+	    if (romanNumeral.contains("CD"))
+	    {
+	        decimal-=100;
+	    }
+	    if (romanNumeral.contains("CM"))
+	    {
+	        decimal-=100;
+	    }
+	    return decimal;
+	}
+
+	public static void main(String[] args) {
+	 	RomanNumeral roman = new RomanNumeral();
+	 	System.out.println(roman.convierte(m));
+	 	System.out.println(roman.convierte(v));
+	 	System.out.println(roman.convierte(vi));
+	 	System.out.println();
+	 	System.out.println(roman.convierte(iv));
+	 	System.out.println(roman.convierte(xii));
+		System.out.println(roman.convierte(ix));
+	 	System.out.println(xxx);
+	 	System.out.println(roman.convierte(m));
+	 	System.out.println(roman.convierte(d));
+	 	System.out.println(roman.convierte(cd));
+		System.out.println();
+		System.out.println(roman.convierte(h));
+		System.out.println(roman.convierte(52));
+	}
+}
