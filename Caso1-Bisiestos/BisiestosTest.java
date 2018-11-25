588470520e0e240c13034b03cf07a0ba18f8bde2
malozanom <ma.lozanom@alumnos.urjc.es>
2018-11-25 18:39:13 +0100

    Añadida parte 2 (grafos

--- Caso1-Bisiestos/BisiestosTest.java
+++ Caso1-Bisiestos/BisiestosTest.java
@@ -12,6 +12,7 @@ public class BisiestosTest
 
 	// Test para comprobar los años que son bisiestos según el bloque b1:
 	// "Años divisibles entre 4 pero no entre 100"
+	// Recorre el camino de prueba i = [1, 3, 4]
 	@Test
 	public void testParaAñoBisiestoSegunB1()
 	{
@@ -22,6 +23,7 @@ public class BisiestosTest
 
 	// Test para comprobar los años que son bisiestos según el bloque b2:
 	// "Años divisibles entre 4, entre 100 y entre 400"
+	// Recorre el camino de prueba i = [1, 3, 4]
 	@Test
 	public void testParaAñoBisiestoSegunB2()
 	{
@@ -32,6 +34,7 @@ public class BisiestosTest
 
 	// Test para comprobar los años que no son bisiestos según el bloque b3:
 	// "Resto de años"
+	// Recorre el camino de prueba ii = [1, 3, 5]
 	@Test
 	public void testParaAñoNoBisiestoSegunB3()
 	{
@@ -42,6 +45,7 @@ public class BisiestosTest
 
 	// Test para comprobar que un año es inválido según el bloque b4:
 	// "Años inválidos (números negativos y el 0)."
+	// Recorre el camino de prueba iii = [1, 2]
 	@Test (expected = RuntimeException.class)
 	public void testParaAñoInvalido()
 	{
